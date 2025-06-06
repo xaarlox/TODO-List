9cd46b0b22efcb55aa1b9d626f33b35cc3e36cbc
Valeriia Fedorenko <xaarlox@gmail.com>
2025-06-06 17:05:58 +0300

    Added dependencies (data module)

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -36,6 +37,22 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+
+    //Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    //Hilt - Dagger
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.hilt.android.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
