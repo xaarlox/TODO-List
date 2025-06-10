d7114ab1c73d59b14999a1e9b4e327d8d9f00524
Valeriia Fedorenko <xaarlox@gmail.com>
2025-06-10 16:36:53 +0300

    UI layer (structure)

---
+++ app/src/main/java/com/xaarlox/todo_list/ui/TodoNavHost.kt
@@ -0,0 +1,32 @@
+package com.xaarlox.todo_list.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.xaarlox.todo_list.ui.screens.EditTodoScreen
+import com.xaarlox.todo_list.ui.screens.LoadingScreen
+import com.xaarlox.todo_list.ui.screens.TodosScreen
+
+@Composable
+fun TodoNavHost(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = TodoScreen.Loading.name
+) {
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(route = TodoScreen.Loading.name) {
+            LoadingScreen()
+        }
+        composable(route = TodoScreen.Todos.name) {
+            TodosScreen()
+        }
+        composable(route = TodoScreen.Edit.name) {
+            EditTodoScreen()
+        }
+    }
+}
+
+enum class TodoScreen {
+    Loading, Todos, Edit
+}
---
+++ app/src/main/java/com/xaarlox/todo_list/ui/screens/TodosScreen.kt
@@ -0,0 +1,9 @@
+package com.xaarlox.todo_list.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TodosScreen() {
+    Text("Todos screen")
+}
