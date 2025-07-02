afdd40590a5654dbefedfa14d0cd76f2377a5675
Valeriia Fedorenko <xaarlox@gmail.com>
2025-07-02 17:32:21 +0300

    Add instrumented UI tests for EditTodoScreen

---
+++ app/src/androidTest/java/com/xaarlox/todo_list/ui/screens/EditTodoScreenTest.kt
@@ -0,0 +1,98 @@
+package com.xaarlox.todo_list.ui.screens
+
+import androidx.compose.ui.test.isToggleable
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.xaarlox.todo_list.ui.util.UiEvent
+import com.xaarlox.todo_list.ui.viewmodels.mvi.EditTodoIntent
+import com.xaarlox.todo_list.ui.viewmodels.mvi.EditTodoState
+import com.xaarlox.todo_list.ui.viewmodels.mvi.EditTodoViewModel
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class EditTodoScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var viewModel: EditTodoViewModel
+    private lateinit var stateFlow: MutableStateFlow<EditTodoState>
+    private lateinit var effectFlow: MutableSharedFlow<UiEvent>
+    private lateinit var onPopBackStack: () -> Unit
+
+    @Before
+    fun setUp() {
+        val initialState = EditTodoState(
+            id = 1,
+            title = "Test title",
+            description = "Test description",
+            isDone = false,
+            isLoading = true
+        )
+        stateFlow = MutableStateFlow(initialState)
+        effectFlow = MutableSharedFlow()
+        viewModel = mockk(relaxed = true)
+        every { viewModel.state } returns stateFlow
+        every { viewModel.uiEffect } returns effectFlow
+        onPopBackStack = mockk(relaxed = true)
+        composeTestRule.setContent {
+            EditTodoScreen(
+                viewModel = viewModel,
+                onPopBackStack = onPopBackStack
+            )
+        }
+    }
+
+    @Test
+    fun screen_shows_all_fields() {
+        composeTestRule.onNodeWithText("Test title").assertExists()
+        composeTestRule.onNodeWithText("Test description").assertExists()
+        composeTestRule.onNodeWithText("Mark as complete").assertExists()
+        composeTestRule.onNodeWithContentDescription("Save").assertExists()
+    }
+
+    @Test
+    fun typing_title_triggers_intent() {
+        composeTestRule.onNodeWithText("Test title").performTextInput(" new")
+        verify { viewModel.onIntent(match { it is EditTodoIntent.OnTitleChanged }) }
+    }
+
+    @Test
+    fun typing_description_triggers_intent() {
+        composeTestRule.onNodeWithText("Test description").performTextInput(" more")
+        verify { viewModel.onIntent(match { it is EditTodoIntent.OnDescriptionChanged }) }
+    }
+
+    @Test
+    fun clicking_checkbox_triggers_intent() {
+        composeTestRule.onNode(isToggleable()).performClick()
+        verify { viewModel.onIntent(match { it is EditTodoIntent.OnIsDoneChanged }) }
+    }
+
+    @Test
+    fun clicking_FAB_shows_snackbar_when_todo_saved() {
+        composeTestRule.onNodeWithContentDescription("Save").performClick()
+        verify { viewModel.onIntent(EditTodoIntent.OnSaveTodoClick) }
+        runBlocking {
+            effectFlow.emit(UiEvent.ShowSnackBar("Todo saved!"))
+        }
+        composeTestRule.onNodeWithText("Todo saved!").assertExists()
+    }
+
+    @Test
+    fun navigates_back_when_event_triggered() {
+        runBlocking {
+            effectFlow.emit(UiEvent.PopBackStack)
+        }
+        verify { onPopBackStack.invoke() }
+    }
+}
